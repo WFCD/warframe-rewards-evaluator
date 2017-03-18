3ad5197b586aff5d314ec23a56066493af97ade1
Clemens Himmer <clemens.himmer@hotmail.com>
2017-03-18 01:14:37 +0100

    allow for inexactly in item name strings

--- src/js/utils/ScreenEvaluator.ts
+++ src/js/utils/ScreenEvaluator.ts
@@ -2,26 +2,30 @@ import {Item} from '../Api';
 import {IState} from '../reducers';
 import {PriceEvaluator} from "./PriceEvaluator";
 import {Api, ItemTradeDetails} from "../Api";
-
+import * as stringSimilarity from "string-similarity";
+ 
 export class ScreenEvaluator {
+    
     private static getItemBySimilarName(name: string, state: IState): Item {
-        // HACK fuck typings, use find...
-        console.log(name);
-        console.log((state.items as any).find(item => item.item_name === name));
-        return state.items.filter(item => item.item_name === name)[0];
+        // Find best match to scraped name
+        const allNames = state.items.map(item => item.item_name);
+        const bestMatch = stringSimilarity.findBestMatch(name, allNames).bestMatch.target;
+
+        // REVIEW Maybe compile to es6? is that ok with electron, or not? Then we can use filter instead of .. this.
+        return state.items.filter(item => item.item_name === bestMatch)[0];
     }
 
     public static async processCurrentScreen(state: IState) {
         // TODO scrape this from screen
         const similarItemNames = [
-            'Akbronco Prime Blueprint',
-            'Warrior',
-            'Night Stalker',
-            'Bo Prime Ornament'
+            'Akbronco Prime Blueprind',
+            'Warrio',
+            'Night Stalken',
+            'Bo Prime Prnament'
         ];
         const correspondingItems = similarItemNames.map(similarItemName => {
             return this.getItemBySimilarName(similarItemName, state);
-        })
+        });
         let correspondingItemTradeDetails: ItemTradeDetails[] = [];
         // Use a old-school loop to get around asynch await error in forEach functor
         
@@ -31,11 +35,11 @@ export class ScreenEvaluator {
         }
         
         correspondingItemTradeDetails.forEach((itemTradeDetail, index) => {
-            const itemName = similarItemNames[index];
+            const item = correspondingItems[index];
             const stats = PriceEvaluator.getAllStatistics(itemTradeDetail);
             
             console.log('');
-            console.log('Some stats for "' + itemName + '"!');
+            console.log('Some stats for "' + item.item_name + '"!');
 
             console.log('Minimal online selling price: ' + stats.minimalOnlineSalePrice);
             console.log('Average online selling price: ' + stats.averageOnlineSalePrice);
